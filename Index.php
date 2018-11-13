02543b004ccc62aca31a8f5164b288c7a96649d4
Raul Montenegro <draul99@gmail.com>
2018-11-13 01:13:15 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <style media="screen">
+        .h2{
+            color: skyblue;
+        }
+
+        .btn{
+            box-shadow: 2px 2px 5px rgb(27, 29, 28);
+        }
+
+        .img{
+            box-shadow: 2px 2px 5px rgb(27, 29, 28);
+        }
+
+    </style>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>C.B.I Compare Business information</title>
+  </head>
+  <body>
+  <form class="container text-center" action="php/Sectores.php" method = "POST">
+      <div class="image text-center">
+            <img class="img img-fluid" src="imagenes/cbi.jpg" width="700px">
+      </div>
+      <div class="titles">
+          <h3 class="h3 text-center mt-2" style="color: rgb(62, 108, 199)"> <em> Comparar empresas nunca fue tan fácil </em> </h3>
+          <h2 class="h2 text-center mt-2" style="color: rgb(62, 108, 199); font-size: 52px;"> C. B. I. </h2>
+          <label class="h2 text-center" style="color: rgb(62, 108, 199); font-size: 12px;"> Compare Bussines Information </label>
+      </div>
+      <div class="row">
+        <div class="col text-center">
+            <a href="php/Sectores.php" class="btn btn-primary center-block mt-2" role = "button" style ="width:350px;"> START </a>
+        </div>
+      </div>
+  </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
